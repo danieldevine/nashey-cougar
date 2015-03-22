fe7b7760eddeb3f8639dcfe5563899ab48b7d118
Dan Devine <dan@blackroad.ie>
2015-03-22 23:20:39 +0000

    tidy up
    beautify the junk

--- content_script.js
+++ content_script.js
@@ -1,45 +1,39 @@
 walk(document.body);
 
-function walk(node)
-{
-	// I stole this function from https://github.com/panicsteve/cloud-to-butt
-    // who stole it from here:
-	// http://is.gd/mwZp7E
+function walk(node) {
+  // I stole this function from https://github.com/panicsteve/cloud-to-butt
+  // who stole it from here:
+  // http://is.gd/mwZp7E
 
-	var child, next;
+  var child, next;
 
-	switch ( node.nodeType )
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child )
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
+  switch (node.nodeType) {
+  case 1: // Element
+  case 9: // Document
+  case 11: // Document fragment
+    child = node.firstChild;
+    while (child) {
+      next = child.nextSibling;
+      walk(child);
+      child = next;
+    }
+    break;
 
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
+  case 3: // Text node
+    handleText(node);
+    break;
+  }
 }
 
-function handleText(textNode)
-{
-	var v = textNode.nodeValue;
+function handleText(textNode) {
+  var v = textNode.nodeValue;
 
-	v = v.replace(/\bNicolas Cage\b/g, "Nashey Cougar");
-	v = v.replace(/\bNicholas Cage\b/g, "Nashey Cougar");
-	v = v.replace(/\bnicolas cage\b/g, "Nashey Cougar");
-	v = v.replace(/\bnicholas cage\b/g, "Nashey Cougar");
-    v = v.replace(/\bNicolas Coppola\b/g, "Nashey Cougar");
-    v = v.replace(/\bnicolas coppola\b/g, "Nashey Cougar");
+  v = v.replace(/\bNicolas Cage\b/g, "Nashey Cougar");
+  v = v.replace(/\bNicholas Cage\b/g, "Nashey Cougar");
+  v = v.replace(/\bnicolas cage\b/g, "Nashey Cougar");
+  v = v.replace(/\bnicholas cage\b/g, "Nashey Cougar");
+  v = v.replace(/\bNicolas Coppola\b/g, "Nashey Cougar");
+  v = v.replace(/\bnicolas coppola\b/g, "Nashey Cougar");
 
-	textNode.nodeValue = v;
+  textNode.nodeValue = v;
 }
-
-
