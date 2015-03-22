6e2ab1e0d443a064460f70396b92ee9953e93423
Dan Devine <dan@blackroad.ie>
2015-03-22 18:57:14 +0000

    Initial Commit
    This is the initial commit of the junk.

---
+++ README.md
@@ -0,0 +1,14 @@
+#Nashey Cougar
+
+
+Chrome extension that replaces occurrences of 'Nicolas Cage' with 'Nashey Cougar'
+
+This is "inspired" by [cloud-to-butt](https://github.com/panicsteve/cloud-to-butt) by [Steven Frank](https://github.com/panicsteve)
+
+##all because
+[of this](https://www.youtube.com/watch?v=kX30tdH0xhs)
+
+##Installation
+In Chrome, choose Window > Extensions.
+Drag nashey-cougar.crx into the page that appears.
+
---
+++ content_script.js
@@ -0,0 +1,42 @@
+walk(document.body);
+
+function walk(node)
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+
+	var child, next;
+
+	switch ( node.nodeType )
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child )
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode)
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\bNicolas Cage\b/g, "Nashey Cougar");
+	v = v.replace(/\bNicholas Cage\b/g, "Nashey Cougar");
+	v = v.replace(/\bnicolas cage\b/g, "Nashey Cougar");
+	v = v.replace(/\bnicholas cage\b/g, "Nashey Cougar");
+
+	textNode.nodeValue = v;
+}
+
+
